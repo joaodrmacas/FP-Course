ce232a94438a00a28741e549d2bbe4795bd86422
Massas <joaomacas02@gmail.com>
2021-10-20 08:07:18 -0700

    Cap 1 e 2

---
+++ 1.py
@@ -0,0 +1,56 @@
+#-----------------------------------------------------------------1.1--------------------------------------------------------------------------
+
+def has_surto (string):
+    if len(string)>1:
+        for i in range(0,len(string)-1):
+            if string[i].islower() and string[i+1].isupper():
+                if string[i] == string[i+1].lower():
+                    return True,i
+            elif string[i].isupper() and string[i+1].islower():
+                if string[i] == string[i+1].upper():
+                    return True,i
+        else:
+            return False,i
+    else:
+        return False,1
+
+def convertTuple(tup):
+    str = ""
+    for i in tup:
+        str = str + i
+    return str
+
+def corrigir_palavra(string):
+    tuplex = string
+    t = tuple(string)
+    loop,i = has_surto(string)
+    while(loop):
+        t = tuple(tuplex)
+        tuplex = t[:i] + t[i+2:]
+        tuplex = convertTuple(tuplex)
+        loop,i = has_surto(tuplex)
+    return tuplex
+
+#-----------------------------------------------------------------1.2--------------------------------------------------------------------------
+
+def eh_anagrama(string1,string2):
+    string1 = string1.upper()
+    string2 = string2.upper()
+    if(string1 == string2):
+        return False
+    return sorted(string1) == sorted(string2)
+
+#-----------------------------------------------------------------1.3--------------------------------------------------------------------------
+
+def corrigir_doc(text): #comparar com todas as palavras anteriores
+    doc = text.split(" ")
+    for i in range(len(doc)):
+       doc[i] = corrigir_palavra(doc[i])
+    for j in range(len(doc)-2):
+        if eh_anagrama(doc[j],doc[j+1]):
+            doc.remove(doc[j+1]) 
+    return doc
+
+print(corrigir_doc("BuAaXOoxiIKoOkggyrFfhHXxR duJjUTtaCcmMtaAGga \
+eEMmtxXOjUuJQqQHhqoada JlLjbaoOsuUeYy cChgGvValLCwMmWBbclLsNn \
+LyYlMmwmMrRrongTtoOkyYcCK daRfFKkLlhHrtZKqQkkvVKza data tada"))
---
+++ 2.py
@@ -0,0 +1,55 @@
+#-----------------------------------------------------------------2.1--------------------------------------------------------------------------
+
+board = [[0,0,0,0,0],[0,1,2,3,0],[0,4,5,6,0],[0,7,8,9,0],[0,0,0,0,0]]
+
+def obter_posicao (direction, position):
+    
+    if direction == "C":
+        for i in range(0,5):
+            for j in range(0,5):
+                if board[i][j] == position and board[i-1][j]!=0:
+                    new_pos = board[i-1][j]
+                    return new_pos
+    
+    elif direction == "B":
+        for i in range(0,5):
+            for j in range(0,5):
+                if board[i][j] == position and board[i+1][j]!=0:
+                    new_pos = board[i+1][j]
+                    return new_pos
+    
+    elif direction == "D":
+        for i in range(0,5):
+            for j in range(0,5):
+                if board[i][j] == position and board[i][j+1]!=0:
+                    new_pos = board[i][j+1]
+                    return new_pos
+
+    elif direction == "E":
+        for i in range(0,5):
+            for j in range(0,5):
+                if board[i][j] == position and board[i][j-1]!=0:
+                    new_pos = board[i][j-1]
+                    return new_pos
+    
+    return position
+
+#-----------------------------------------------------------------2.2--------------------------------------------------------------------------
+
+def obter_digito (sequence, position):
+    t_sequence = tuple(sequence)
+    for i in range(len(t_sequence)):
+        position = obter_posicao(t_sequence[i], position)
+    return position
+
+#-----------------------------------------------------------------2.3--------------------------------------------------------------------------
+
+def obter_pin(sequence_list):
+    pin = ()
+    pin += (obter_digito(sequence_list[0],5),)
+    for i in range(1,len(sequence_list)):
+        pin += (obter_digito(sequence_list[i],pin[i-1]),)
+    return pin
+
+t = ("CEE", "DDBBB", "ECDBE", "CCCCB")
+print(obter_pin(t))
