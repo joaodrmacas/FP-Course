ff9215368d15a5a055fd5cd7c30d0a64bbb73db8
Massas <joaomacas02@gmail.com>
2021-10-20 12:39:49 -0700

    update checkCifra

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
 
-def checkCifra(cifra):
+def checkCifra2(cifra):
     for i in range(len(cifra)):
         if not cifra[i].isalpha():
             return False
@@ -7,32 +7,43 @@ def checkCifra(cifra):
         return True
     return False
 
-def checkControl(control):
-    if control[0] == "[" and control[-1] == "]" and len(control) == 7:
-        for i in range(1,6):
-            if not control[i].isalpha():
+def checkCifra(cifra):
+    cifra_array = cifra.split("-")
+    for j in range(len(cifra_array)):
+        for i in range(len(cifra_array[j])):
+            if not cifra_array[j][i].isalpha():
                 return False
-        if control.islower():
-            return True
-    return False
+        if len(cifra_array[j]) < 1 and not cifra_array[j].islower():
+            return False
+    return True
 
-# def checkSecurity(security):
-#     for i in range(len(security)):
-#         if security[i].isinteger() and security[i] > 0:
-#             return False
-#     if len(security) >= 2:
-#         return True
+print(checkCifra("o2-a"))
+# def checkControl(control):
+#     if control[0] == "[" and control[-1] == "]" and len(control) == 7:
+#         for i in range(1,6):
+#             if not control[i].isalpha():
+#                 return False
+#         if control.islower():
+#             return True
 #     return False
+
+# # def checkSecurity(security):
+# #     for i in range(len(security)):
+# #         if security[i].isinteger() and security[i] > 0:
+# #             return False
+# #     if len(security) >= 2:
+# #         return True
+# #     return False
     
-# print(checkSecurity(("a",2,3)))
+# # print(checkSecurity(("a",2,3)))
 
-def eh_entrada(entry):
-    if type(entry) == tuple and len(entry) == 3:
-        cifra,control,security = entry[0], entry[1], entry[2]
-        if type(cifra) == str and type(control) == str and type(security)==tuple:
-            if checkCifra(cifra) and checkControl(control):
-                return True
-    #raise ValueError("Entry not supported.")
-    return False
+# def eh_entrada(entry):
+#     if type(entry) == tuple and len(entry) == 3:
+#         cifra,control,security = entry[0], entry[1], entry[2]
+#         if type(cifra) == str and type(control) == str and type(security)==tuple:
+#             if checkCifra(cifra) and checkControl(control):
+#                 return True
+#     #raise ValueError("Entry not supported.")
+#     return False
 
-print(eh_entrada(("ola-a","[olaaa]",(1,2,3))))
+# print(eh_entrada(("ola-a","[olaaa]",(1,2,3))))
